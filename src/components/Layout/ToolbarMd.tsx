d9bdf0a213538680d0100c6195dc028fb192675e
Lubos Fukas <lubos.fukas@gmail.com>
2022-04-06 11:29:49 +0200

    fix: :bug: toolbar height

--- src/components/Layout/ToolbarMd.tsx
+++ src/components/Layout/ToolbarMd.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import styled from '@emotion/styled'
 import { AppBar, Container, Stack, Toolbar } from '@mui/material'
 
 import { Menu } from './Menu'
 import { routes } from '../../Router'
 
+const StyledContainer = styled(Container)`
+    padding-top: 0 !important;
+    padding-bottom: 0 !important;
+`
+
 export const ToolbarMd = () => (
     <AppBar position="static">
         <Toolbar disableGutters>
-            <Container>
+            <StyledContainer>
                 <Stack direction="row" spacing={3}>
                     {Object.keys(routes).map((media) => (
                         <Menu
@@ -17,7 +22,7 @@ export const ToolbarMd = () => (
                         />
                     ))}
                 </Stack>
-            </Container>
+            </StyledContainer>
         </Toolbar>
     </AppBar>
 )
